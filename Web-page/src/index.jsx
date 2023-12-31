511813323ac7ca0710d3556e4e07a5ad06ba5109
Juanma <jfrauditamacia@gmail.com>
2023-12-31 13:28:29 -0400

    cambio de nombres Index

--- Web-page/src/index.jsx
+++ Web-page/src/index.jsx
@@ -5,7 +5,7 @@ export default function App() {
       <div className="pt-8 flex items-center justify-center">
           <img className="w-16 sm:w-40 2xl:w-96" src="logo.png" alt="Logo"/>
       </div>
-      <div className="flex flex-col items-center justify-center pt-12 lg:px-96">
+      <div className="flex flex-col items-center justify-center pt-12 lg:px-88">
 
         <div className="flex justify-center items-center m-3 w-56 lg:w-full h-12 lg:h-28 bg-gray-900 hover:bg-gray-800 rounded-full"> 
           <a className="text-white font-bold text-xl lg:text-5xl">
