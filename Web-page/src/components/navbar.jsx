d1c8514c25143823309224693c13d7136f997474
Juanma <jfrauditamacia@gmail.com>
2024-01-01 16:01:17 -0400

    agregando json y navbar

--- Web-page/src/components/navbar.jsx
+++ Web-page/src/components/navbar.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
+import { useState } from 'react';
+import listP from './listP.json'
 
 export default function Navbar () {
+
+  const [isOpen, setIsOpen] = useState (false)
+
   return (
+
     <>
-    <nav>
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex-shrink-0">
-            <img className="h-8" src="logo.png" alt="Logo" />
-          </div>
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              <a href="#" className="text-gray-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-              <a href="#" className="text-gray-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-              <a href="#" className="text-gray-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+      <nav>
+        <div className="pt-8 max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0">
+              <img className=" h-12" src="logo.png" alt="Logo" />
+            </div>
+            <div className="hidden md:block">
+              <div className="mr-10 flex items-baseline space-x-4">
+                <div className='flex'>
+                    <a href="#" onClick={() => setIsOpen((prev) => !prev )} className="text-gray-500 hover:text-gray-800 hover:duration-300 duration-100 px-3 py-2 rounded-md text-sm font-medium active:text-gray-300"> 
+                    Photograhy
+                    </a>
+                    {isOpen && (
+                      <div className='top-16 absolute flex flex-col items-center justify-center'>
+                        {listP.map((item) => (
+                          <div>
+                            <a className='text-gray-500 hover:text-gray-800 rounded-md text-sm font-medium active:text-gray-300 hover:duration-300 duration-100'> 
+                              {item.folder} 
+                            </a>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    <div>
+                  </div>  
+                </div>
+
+                <div>
+                  <a href="#" className="text-rose-500/70 hover:text-rose-500 px-3 py-2 rounded-md text-sm font-medium active:text-rose-300 hover:duration-300 duration-100 "> 
+                  About 
+                  </a>
+                </div>
+
+
+                <div>
+                  <a href="#" className="text-gray-500 hover:text-gray-800 hover:duration-300 duration-100 px-3 py-2 rounded-md text-sm font-medium active:text-gray-300"> 
+                  Contact 
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   )
 }
