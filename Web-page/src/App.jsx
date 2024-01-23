b75f7df077d5a10cd22a1fa626d2f78c9c758ca8
Juanma <jfrauditamacia@gmail.com>
2024-01-22 22:28:50 -0400

    quitando API imagekit

--- Web-page/src/App.jsx
+++ Web-page/src/App.jsx
@@ -5,15 +5,6 @@ import Home from "./pages/Home"
 import HomeP from "./pages/home photography"
 import HomeC from "./pages/home code"
 import HomeD from "./pages/home design"
-import { IKImage, IKVideo, IKContext, IKUpload } from 'imagekitio-react'
-
-const urlEndpoint = '<https://ik.imagekit.io/f10c3to17/>'
-const publicKey = '<public_uOVFRhFppXpYrQdII5ome8vMYy0=>'; 
-const authenticator = ()=>{
-  return new Promise((resolve,reject)=>{
-    resolve({signature,token,expiry})
-  })
-};
 
 
 export default function App(){
--- Web-page/src/pages/Home.jsx
+++ Web-page/src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { IKContext, IKImage } from "imagekitio-react";
 
 
 export default function Home() {
   
     return (
-      <>
+
+      
       <body className="bg-1e1e1e w-screen h-screen">
+
         <div className="pt-8 flex items-center justify-center">
       
-          <IKContext publicKey="public_uOVFRhFppXpYrQdII5ome8vMYy0=" urlEndpoint="https://ik.imagekit.io/f10c3to17/" transformationPosition="path" authenticationEndpoint="http://www.yourserver.com/auth"/>
-            
-          <IKImage path=" https://ik.imagekit.io/f10c3to17/home%20photos/Logo/Logo%20SVG.svg?updatedAt=1704316803289 " transformation={[{"height": "300", "width": "400"}]} />
+          <img src="logo.png" className="flex justify-center w-20 lg:w-40 2xl:w-80" />
 
         </div>
-        <div className="flex flex-col items-center justify-center pt-12 lg:px-96">
+        <div className="flex flex-col items-center justify-center pt-8 lg:px-96">
   
           <div className="flex justify-center items-center m-2 w-56 lg:w-80 2xl:m-12 2xl:w-full h-12 lg:h-16 2xl:h-24 bg-eacdc2 hover:bg-ffe8d6 rounded-full"> 
             <a className="text-1e1e1e font-bold lg:text-2xl 2xl:text-5xl" href="/HomeP">
@@ -35,7 +34,6 @@ export default function Home() {
           </div>
         </div>
       </body>
-    </>
     )
   }
 
