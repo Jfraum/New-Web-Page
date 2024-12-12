ff5b3af6cfccec0fbbc658b5d614dc6bf0a0c30e
Juanma <jfrauditamacia@gmail.com>
2024-12-12 09:29:36 -0400

    adding new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+import flowbitePlugin from 'flowbite/plugin';
 
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    
-      // ...
-      'node_modules/flowbite-react/lib/esm/**/*.js',
-    ],
-    plugins: [
-      // ...
-      require('flowbite/plugin'),
-    ],
+    "node_modules/flowbite-react/lib/esm/**/*.js", // Incluye los archivos necesarios
+  ],
   theme: {
     extend: {
       colors: {
-        'eacdc2': '#eacdc2',
-        '1e1e1e' : '#1e1e1e',
-        'ffe8d6' : '#ffe8d6',
-      }
+        eacdc2: '#eacdc2',
+        '1e1e1e': '#1e1e1e',
+        ffe8d6: '#ffe8d6',
+      },
     },
   },
-  
-  plugins: [],
-}
+  plugins: [
+    flowbitePlugin, // Usa la importación en lugar de require
+  ],
+};
+
 
